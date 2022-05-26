504a511ead7a035ac0aa42f1dd945f51ac4515c7
nima <nimaqanbari1999@gmail.com>
2022-05-25 22:21:57 -0700

    SSR rendering in material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
 import "../styles/globals.css";
 import "../src/font/dana.css";
+import Head from 'next/head';
+import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../src/Theme/default";
 import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../src/createEmotionCache";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createEmotionCache()
+const queryClient = new QueryClient();
+
+function MyApp({ Component,emotionCache = clientSideEmotionCache, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+        <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline/>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
